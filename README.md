8917818af751349f06b00f182c917a8c6b700945
Joachim Byrnes-Shay <jmb464@g.harvard.edu>
2016-09-13 16:47:01 -0400

    first commit, practice

---
+++ README.md
@@ -0,0 +1,6 @@
+#this is another test
+
+9-13-2016
+
+I'm testing push/pull
+
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>test file</title>
+  <meta charset="utf-8">
+  <link href="css/styles.css" rel="stylesheet" type="text/css"?
+</head>
+
+<body>
+  <p>this is the way to go</p>
+  <p>its great!</p>
+</body>
+</html>
+
+
